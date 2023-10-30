37ac033aaa71a5bf3db5b7fdf5219cb5c553b6d1
amir <emirkh165@gmail.com>
2023-10-30 16:44:24 +0330

    project completed

---
+++ app.js
@@ -0,0 +1,15 @@
+const hour = document.querySelector(".hour");
+const mins = document.querySelector(".mins");
+const sec = document.querySelector(".sec");
+
+function timeGetter() {
+  const time = new Date();
+  hour.innerHTML = time.getHours();
+  mins.innerHTML = time.getMinutes();
+  const paddedSeconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+  sec.innerHTML = paddedSeconds;
+}
+
+timeGetter();
+
+setInterval(timeGetter, 1000);
